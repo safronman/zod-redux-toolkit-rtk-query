af86f2e9aac6baf481f35ba117311ae5a51f0471
safronman <safronmanbox@gmail.com>
2025-04-07 15:46:19 +0300

    feat: add RTK query to todolists and tasks - 2

---
+++ src/common/utils/handleError.ts
@@ -0,0 +1,42 @@
+import { setAppErrorAC } from "@/app/app-slice"
+import { ResultCode } from "@/common/enums"
+import { isErrorWithMessage } from "./isErrorWithMessage"
+import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from "@reduxjs/toolkit/query/react"
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>,
+) => {
+  let error = "Some error occurred"
+
+  if (result.error) {
+    switch (result.error.status) {
+      case "FETCH_ERROR":
+      case "PARSING_ERROR":
+      case "CUSTOM_ERROR":
+        error = result.error.error
+        break
+      case 403:
+        error = "403 Forbidden Error. Check API-KEY"
+        break
+      case 400:
+      case 500:
+        if (isErrorWithMessage(result.error.data)) {
+          error = result.error.data.message
+        } else {
+          error = JSON.stringify(result.error.data)
+        }
+        break
+      default:
+        error = JSON.stringify(result.error)
+        break
+    }
+    api.dispatch(setAppErrorAC({ error }))
+  }
+
+  if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
+    const messages = (result.data as { messages: string[] }).messages
+    error = messages.length ? messages[0] : error
+    api.dispatch(setAppErrorAC({ error }))
+  }
+}
